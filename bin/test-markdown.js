be832367a50bfae9c22cd541c24328ae39edfdb1
reggi <thomas@reggi.com>
2015-08-10 16:53:06 -0400

    updated test-markdown bin

--- bin/test-markdown.js
+++ bin/test-markdown.js
@@ -1,45 +1,27 @@
 #!/usr/bin/env node
 var minimist = require('minimist')
-// var _ = require('lodash')
+var path = require('path')
 var argv = minimist(process.argv.slice(2))
 var testMarkdown = require('../test-markdown')
 var pkg = require('../package.json')
-
-var filePath = (argv.file) ? argv.file : argv._.shift()
+var binDoc = require('../bin-doc')
+var markdownPath = (argv.path) ? argv.path : argv._.shift()
 
 var doc = {
-  'name': pkg.name,
+  'name': path.parse(__filename).name,
   'desc': pkg.description,
   'usage': {
-    '<file|directory>': 'File or directory of markdown files to test.'
+    '<path>': 'File or directory of markdown files to test.'
   },
   'options': {
-    'moduleFile': 'Javascript file to build into module.',
-    'moduleName': 'Name of the module. (default: moduleFile name || jsdoc module def name)',
-    'moduleDesc': 'Description of the module (default: jsdoc module def summary || jsdoc module def desc)',
-    'packageSrc': 'Path to superproject package.json file',
-    'localModulesDirName': 'Path to where local modules will build.',
-    'directory': 'Path to directory (defaults: "./")',
-    'buildLinks': 'Array of src, [src], or [src, dst] hard link definitions, from "./" to `local_module`.',
-    'trackDeps': 'Array of src, [src] javascript definitions, from "./" to `local_module`.',
-    'trackDevDeps': 'Function returning array of src, [src] javascript testdefinitions, from "./" to `local_module`.',
-    'postBuildReverseLinks': 'Array of src, [src], or [src, dst] hard link definitions, from `local_module` to "./".',
-    'githubAccessToken': 'Github access token',
-    'githubRepoPrefix': 'Github repo prefix (ex: "node-")',
-    'preventMerge': 'Boolean option for prevent default merge options.'
-  },
-  'optionAliases': {
-    'moduleFile': ['file'],
-    'moduleName': ['name'],
-    'moduleDesc': ['desc'],
-    'packageSrc': ['package', 'pkg']
+    'path': 'File or directory of markdown files to test.',
   }
 }
 
 if (argv.v || argv.version) {
   console.log(pkg.version)
-} else if (filePath) {
-  testMarkdown(filePath)
+} else if (markdownPath) {
+  testMarkdown(markdownPath)
 } else {
-
+  console.log(binDoc(doc))
 }
